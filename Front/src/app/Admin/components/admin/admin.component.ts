aa2cceb97d39e972e1756999f32b8ab6de7f3771
Marta <mperellolo@uoc.edu>
2025-01-24 18:56:03 +0100

    Cambio standart

--- Front/src/app/Admin/components/admin/admin.component.ts
+++ Front/src/app/Admin/components/admin/admin.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
-  standalone: false,
+
   
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
