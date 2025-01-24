b4f52ab24888ea82257cb94fe395f37541d7f991
Marta <mperellolo@uoc.edu>
2025-01-24 17:27:25 +0100

    Arreglo importacion servicio

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LayoutService } from './Shared/services/layout.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'duos';
+  showLayout$ = this.layoutService.showLayout$;
+  constructor(private layoutService: LayoutService) {}
 }
