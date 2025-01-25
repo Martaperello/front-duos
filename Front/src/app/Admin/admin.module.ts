0d73d7129e883a6129273be60710adad6ea2b6fa
Marta <mperellolo@uoc.edu>
2025-01-25 12:40:56 +0100

    Adicionar importacion icons

--- Front/src/app/Admin/admin.module.ts
+++ Front/src/app/Admin/admin.module.ts
@@ -6,6 +6,7 @@ import { SidebarComponent } from './parts/sidebar/sidebar.component';
 import { UsersComponent } from './sections/users/users.component';
 import { ProductsComponent } from './sections/products/products.component';
 import { OrdersComponent } from './sections/orders/orders.component';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -19,7 +20,8 @@ import { OrdersComponent } from './sections/orders/orders.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatIconModule
   ]
 })
 export class AdminModule { }
